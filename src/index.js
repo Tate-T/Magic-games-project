f0b46c387d1b3cecf507397b614e31db2d74695e
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2024-09-24 20:55:31 +0300

    final set up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './js/header';
 import './js/check-year';
 import './js/guess';
-// import './js'; камінь ножиці папір
-// import './js'; калькулятор
+import './js/cnb';
+import './js/calculator';
 import './js/time-calculator';
-// import './js'; динозаврик
+import './js/google-dino';
 import './js/football';
 import './js/three-numbers';
 import './js/our-team';
