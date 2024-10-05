71e30bfb86720929d6ce7c5bacaaacf2193d6a97
i-skachkova <malaja0707@gmail.com>
2024-10-05 20:31:18 +0300

    theme switch

--- src/js/header.js
+++ src/js/header.js
@@ -11,3 +11,18 @@ modeBtn.addEventListener("click", () =>{
     modeArrow.classList.toggle("reverse-arrow");
     modeList.classList.toggle("hidden-mode-list");
 })
+
+let theme = 0;
+themeBtn.addEventListener("click", ()=>{
+    if(theme == 0){
+        document.documentElement.style.setProperty('--black', '#fff');
+        document.documentElement.style.setProperty('--white', 'rgb(28, 28, 28)');
+        theme = 1;
+    }
+    else{
+        document.documentElement.style.setProperty('--black', '#000');
+        document.documentElement.style.setProperty('--white', '#fff');
+        theme = 0;
+    }
+    
+})
