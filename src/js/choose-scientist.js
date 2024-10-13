1fa4e25fea6c0898fb0cbfa90b03165a224ea698
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-10-13 11:53:16 +0300

    done all js

--- src/js/choose-scientist.js
+++ src/js/choose-scientist.js
@@ -18,18 +18,28 @@ chooseScientistBtnArr[0].addEventListener('click', () => {
     chooseScientisImgArr[11].classList.remove('box-hidden')
 })
 
-// chooseScientistBtnArr[1].addEventListener('click', () => {
-//     chooseScientisImgArr.forEach(a => {
-//         a.classList.remove('box-hidden')
-//     })
-//     const arrCopy = [...chooseScientisImgArr]
-//     arrCopy.sort((nextEl,currentEl) => {
-        
-//     })
-//     chooseScientisItemArr.forEach((el, indx) => {
-//         el.append(arrCopy[indx])
-//     })
-// })
+chooseScientistBtnArr[1].addEventListener('click', () => {
+    chooseScientisImgArr.forEach(a => {
+        a.classList.remove('box-hidden')
+    })
+    const arrCopy = [...chooseScientisImgArr]
+    const testArr = []
+    testArr[0] = arrCopy[8]
+    testArr[1] = arrCopy[0]
+    testArr[2] = arrCopy[2]
+    testArr[3] = arrCopy[11]
+    testArr[4] = arrCopy[1]
+    testArr[5] = arrCopy[4]
+    testArr[6] = arrCopy[7]
+    testArr[7] = arrCopy[10]
+    testArr[8] = arrCopy[3]
+    testArr[9] = arrCopy[6]
+    testArr[10] = arrCopy[5]
+    testArr[11] = arrCopy[9]
+    chooseScientisItemArr.forEach((el, indx) => {
+        el.append(testArr[indx])
+    })
+})
 
 chooseScientistBtnArr[2].addEventListener('click', () => {
     chooseScientisImgArr.forEach(a => {
