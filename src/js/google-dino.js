a1313e33842a10a025a872133f2a6c20a3cf5050
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-10-13 20:35:31 +0300

    vavi

--- src/js/google-dino.js
+++ src/js/google-dino.js
@@ -1,26 +1,31 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const gameContainer = document.getElementById("gameContainer");
+const lossCounter = document.getElementById("lossCounter");
+
 let isJumping = false;
 let isAlive = false;
+let lossesCount = 0; 
 
-cactus.style.display = "none";
+cactus.style.display = "none"; 
 
 gameContainer.addEventListener("click", function () {
   if (!isAlive) {
     startGame();
   }
 
-  if (!isJumping) {
+  if (!isJumping && isAlive) {
     jump();
   }
 });
 
 function startGame() {
   isAlive = true;
-  cactus.style.display = "block";
-  cactus.style.animation = "moveCactus 2s infinite linear";
-  collisionCheck = setInterval(checkCollision, 10);
+  cactus.style.display = "block"; 
+  cactus.style.left = ""; 
+  cactus.style.animation = "moveCactus 2s infinite linear"; 
+
+  collisionCheck = setInterval(checkCollision, 10); 
 }
 
 function jump() {
@@ -34,33 +39,57 @@ function jump() {
   }
 }
 
-let collisionCheck = setInterval(function () {
-    if (isAlive) {
-      let dinoTop = parseInt(
-        window.getComputedStyle(dino).getPropertyValue("top")
-      );
-      let cactusLeft = parseInt(
-        window.getComputedStyle(cactus).getPropertyValue("left")
-      );
-  
-      if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        gameOver(); 
-      }
+let collisionCheck = setInterval(checkCollision, 10);
+
+function checkCollision() {
+  if (isAlive) {
+    let dinoTop = parseInt(
+      window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    let cactusLeft = parseInt(
+      window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+      gameOver();
     }
-  }, 10);
-  
-  function gameOver() {
-    clearInterval(collisionCheck);
-    const modal = document.querySelector(".google-hi-backdrop");
-    modal.classList.remove("google-hi-modal-hidden"); 
-    const modalBtn = document.querySelector(".google-hi-modal__submit");
-    const closeHiModal = document.querySelector(".google-hi-modal__close");
-    closeHiModal.addEventListener("click", function () {
-      modal.classList.add("google-hi-modal-hidden");
-      location.reload(); 
-    });
-    modalBtn.addEventListener("click", function () {
-        modal.classList.add("google-hi-modal-hidden");
-        location.reload(); 
-      });
   }
+}
+
+function gameOver() {
+  clearInterval(collisionCheck);
+  isAlive = false; 
+  cactus.style.animation = "none"; 
+
+  lossesCount++; 
+  lossCounter.innerText = lossesCount; 
+
+  const modal = document.querySelector(".google-hi-backdrop");
+  modal.classList.remove("google-hi-modal-hidden");
+
+  const modalBtn = document.querySelector(".google-hi-modal__submit");
+  const closeHiModal = document.querySelector(".google-hi-modal__close");
+
+  closeHiModal.addEventListener("click", function () {
+    modal.classList.add("google-hi-modal-hidden");
+    resetGame();
+  });
+
+  modalBtn.addEventListener("click", function () {
+    modal.classList.add("google-hi-modal-hidden");
+    resetGame();
+  });
+}
+
+function resetGame() {
+  isAlive = false;
+  isJumping = false;
+
+  cactus.style.display = "none"; 
+  cactus.style.animation = "none"; 
+
+  dino.classList.remove("jump");
+
+  
+  cactus.style.left = "100%"; 
+}
